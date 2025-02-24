af47c8cfb723ec4587c75e36d9ae36ed79222734
Matt Henderson <mattcharlesh@gmail.com>
2025-02-23 23:20:03 -0500

    add TypeScript interfaces for Resource, Clip, and Note

---
+++ client/src/types/index.ts
@@ -0,0 +1,30 @@
+export type ResourceType = 'book' | 'podcast' | 'article' | 'audiobook';
+export type ClipType = 'quote' | 'highlight' | 'note';
+
+export interface Resource {
+  id: string;
+  type: ResourceType;
+  dateCreated: string;
+  dateUpdated: string;
+  title: string;
+  content: string;
+  // todo: update to support audio content
+}
+
+export interface Clip {
+  id: string;
+  resourceId: string;
+  type: ClipType;
+  dateCreated: string;
+  dateUpdated: string;
+  content: string;
+}
+
+export interface Note {
+  id: string;
+  resourceId: string;
+  clipId: string | null;
+  dateCreated: string;
+  dateUpdated: string;
+  content: string;
+}
