01233f122d0232deff7f45b863ef943b606eea65
Matt Henderson <mattcharlesh@gmail.com>
2025-02-25 11:41:16 -0500

    feat: setup shadcn ui

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,15 @@
+import { Button } from '@/components/ui/button'
+
 function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-6 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">
+      <h1 className="text-4xl font-bold mb-2 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">
         Welcome to Notaverse
       </h1>
-      <p className="text-lg text-muted-foreground">
+      <p className="text-lg text-muted-foreground mb-8">
         Your personal space for notes and thoughts
       </p>
+      <Button size='lg' className='hover:cursor-pointer text-xl'>Enter</Button>
     </div>
   )
 }
